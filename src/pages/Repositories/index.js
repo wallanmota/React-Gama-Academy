741d50a60b7adeb1f9bc68aa9571e7f7094d07df
wallanmota <wallanmota.13@gmail.com>
2021-08-19 20:35:56 -0300

    link react-router-dom

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -7,7 +7,7 @@ export default function Repositories(){
         let repositoriesName = localStorage.getItem('repositoriesName');
         repositoriesName = JSON.parse(repositoriesName);
         setRepositories(repositoriesName);
-        localStorage.clear();
+        // localStorage.clear();
     }, []);
 
     return(
@@ -20,6 +20,7 @@ export default function Repositories(){
                 )
             })}
         </S.List>
+        <S.linkHome to="/">Voltar</S.linkHome>
         </S.container>
     ) 
 };
--- src/pages/Repositories/styled.js
+++ src/pages/Repositories/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
 export const container = styled.div`
     margin: 0 auto;
@@ -10,21 +11,34 @@ export const Title = styled.h1`
     font-size: 2rem;
     font-family: sans-serif;
     color: #333;
-    `
- 
-    
+`;
 
 export const List = styled.ul`
     list-style:none;
     padding: 0;
     margin: 0;
     font-family: sans-serif;
-`
+`;
 export const ListItem = styled.li`
     margin: .5rem 0;
     background: #000;
     color: #fff;
     padding: .5rem;
     border-radius: 5px;
-`
+`;
+export const linkHome = styled(Link)`
+    display: block;
+    width: 4rem;
+    text-align: center;
+    margin: 2rem auto;
+    background-color: #000;
+    color: #fff;
+    padding: .5rem 0;
+    border-radius: 5px;
+    text-decoration: none;
 
+    &:hover{
+        text-decoration: underline;
+    }
+
+`
